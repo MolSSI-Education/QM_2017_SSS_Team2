fe1364d6827205dbeaac0ec45942a15890bb4780
aerask <aerask@umich.edu>
2017-07-25 17:43:40 -0400

    new test

--- tests/test_math.py
+++ tests/test_math.py
@@ -2,19 +2,19 @@
 Testing for the math.py module.
 """
 
-import lbq as lbq #runs a test on the lbq in the same directory as tests/
+import qm2 #runs a test on the qm2 in the same directory as tests/
 import pytest
 
 def test_add():
-    assert lbq.math.add(5, 2) == 7
-    assert lbq.math.add(2, 5) == 7
+    assert qm2.math.add(5, 2) == 7
+    assert qm2.math.add(2, 5) == 7
 
 def test_mult():
-    assert lbq.math.mult(5, 2) == 10
-    assert lbq.math.mult(2, 5) == 10
+    assert qm2.math.mult(5, 2) == 10
+    assert qm2.math.mult(2, 5) == 10
 
 def test_greater():
-    assert lbq.math.greater(5, 2) == 5
-    assert lbq.math.greater(2, 5) == 5
-    assert lbq.math.greater(5, 5) == 5
+    assert qm2.math.greater(5, 2) == 5
+    assert qm2.math.greater(2, 5) == 5
+    assert qm2.math.greater(5, 5) == 5
 
