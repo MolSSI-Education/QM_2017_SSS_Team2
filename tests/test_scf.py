5e33b645abb0a5261dccbe422acc75b2c6f90396
aerask <aerask@umich.edu>
2017-07-26 15:50:12 -0400

    initial test_scf

---
+++ tests/test_scf.py
@@ -0,0 +1,21 @@
+"""
+Testing for the scf.py module.
+"""
+
+import qm2
+import pytest
+import test_data
+
+def test_buildgeom(mol, E_nuc):
+#    assert qm2.scf.scfrun(mol) == E_nuc
+    assert False
+
+def test_diag(F, eps, C):
+#    assert qm2.scf.diag(F) == eps, C
+    assert False
+
+def test_scfrun(mol, E_total):
+#    assert qm2.scf.scfrun(mol) == E_total
+    assert False
+
+
