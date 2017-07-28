2fd286b49386a99e0138bb941e35eb874ffa41f7
aerask <aerask@umich.edu>
2017-07-28 17:40:09 -0400

    Tests probably good to go now

--- tests/test_scf.py
+++ tests/test_scf.py
@@ -6,16 +6,18 @@ import qm2
 import pytest
 import test_data
 
-def test_build_geom(mol, E_nuc):
-#    assert qm2.scf.build_geom(mol) == E_nuc
-    assert False
+@pytest.mark.parametrize("molstr,E_nuc",test_build_geom_data)
+def test_build_geom(molstr, E_nuc):
+    mol = qm2.scf.build_geom(molstr)
+    assert mol.nuclear_repulsion_energy() == E_nuc
 
-def test_diag(F, eps, C):
+#@pytest.mark.parametrize("",)
+#def test_diag(F, eps, C):
+#    eps, C = qm2.scf.diag(F)
 #    assert qm2.scf.diag(F) == eps, C
-    assert False
+#    assert False
 
-def test_scf_run(mol, E_total):
-#    assert qm2.scf.scf_run(mol) == E_total
-    assert False
-
+@pytest.mark.parametrize("molstr,E_total",test_E_scf_data)
+def test_run_scf(molstr, E_total):
+    assert qm2.scf.run_scf(molstr) == E_total
 
