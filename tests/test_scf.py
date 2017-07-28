4bce242475545d80900faa6484ba9aa28f936e0e
aerask <aerask@umich.edu>
2017-07-28 17:03:40 -0400

    tests/test_scf.py

--- tests/test_scf.py
+++ tests/test_scf.py
@@ -6,16 +6,16 @@ import qm2
 import pytest
 import test_data
 
-def test_buildgeom(mol, E_nuc):
-#    assert qm2.scf.scfrun(mol) == E_nuc
+def test_build_geom(mol, E_nuc):
+#    assert qm2.scf.build_geom(mol) == E_nuc
     assert False
 
 def test_diag(F, eps, C):
 #    assert qm2.scf.diag(F) == eps, C
     assert False
 
-def test_scfrun(mol, E_total):
-#    assert qm2.scf.scfrun(mol) == E_total
+def test_scf_run(mol, E_total):
+#    assert qm2.scf.scf_run(mol) == E_total
     assert False
 
 
