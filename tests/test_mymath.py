61e60978e7afe34de1c84a309fe31cac089edbf9
Kirk Pearce <kcpearce@vt.edu>
2017-07-30 20:41:02 -0400

    More math vs. mymath changes

--- tests/test_mymath.py
+++ tests/test_mymath.py
@@ -4,44 +4,44 @@ Testing for the math.py module.
 
 import qm2  # runs a test on the qm2 in the same directory as tests/
 import pytest
-import test_data # test sets for math functions stored in test_data.py
+import test_data # test sets for mymath functions stored in test_data.py
 
 
-# Test math.add function
+# Test mymath.add function
 @pytest.mark.parametrize("a,b,expected", test_data.test_add_data)
 def test_add(a, b, expected):
-    assert qm2.math.add(a, b) == expected
-    assert qm2.math.add(b, a) == expected
+    assert qm2.mymath.add(a, b) == expected
+    assert qm2.mymath.add(b, a) == expected
 
 
-# Test math.sub function
+# Test mymath.sub function
 @pytest.mark.parametrize("a,b,expected", test_data.test_sub_data)
 def test_sub(a, b, expected):
-    assert qm2.math.sub(a, b) == expected
-    assert qm2.math.sub(b, a) == -expected
+    assert qm2.mymath.sub(a, b) == expected
+    assert qm2.mymath.sub(b, a) == -expected
 
 
-# Test math.mult function
+# Test mymath.mult function
 @pytest.mark.parametrize("a,b,expected", test_data.test_mult_data)
 def test_mult(a, b, expected):
-    assert qm2.math.mult(a, b) == expected
-    assert qm2.math.mult(b, a) == expected
+    assert qm2.mymath.mult(a, b) == expected
+    assert qm2.mymath.mult(b, a) == expected
 
 
-# Test math.div function
+# Test mymath.div function
 @pytest.mark.parametrize("a,b,expected", test_data.test_div_data)
 def test_div(a, b, expected):
-    assert qm2.math.div(a, b) == expected
+    assert qm2.mymath.div(a, b) == expected
 
 
-# Test math.mod function
+# Test mymath.mod function
 @pytest.mark.parametrize("a,b,expected", test_data.test_mod_data)
 def test_mod(a, b, expected):
-    assert qm2.math.mod(a, b) == expected
+    assert qm2.mymath.mod(a, b) == expected
 
 
-# Test math.greater function
+# Test mymath.greater function
 @pytest.mark.parametrize("a,b,expected", test_data.test_greater_data)
 def test_greater(a, b, expected):
-    assert qm2.math.greater(a, b) == expected
-    assert qm2.math.greater(b, a) == expected
+    assert qm2.mymath.greater(a, b) == expected
+    assert qm2.mymath.greater(b, a) == expected
